58639f0a56bdf9805b411feaf29d030329d24051
muhammed769 <70298773+muhammed769@users.noreply.github.com>
2021-10-25 15:07:18 +0300

    Add files via upload

---
+++ MahalleniKoru 17-KameraTitremesi VE Kod Revize İşlemleri/MermiKutusu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MermiKutusu : MonoBehaviour
+{
+    string[] silahlar =
+        {
+            "Magnum",
+            "Pompali",
+            "Sniper",
+            "Taramali"
+        };
+
+    int[] mermiSayisi =
+    {
+            10,
+            20,
+            5,
+            30
+
+    };
+
+    public List<Sprite> Silah_resimleri = new List<Sprite>();
+    public Image Silahin_resimi;
+
+
+    public string Olusan_Silahin_Turu;
+    public int Olusan_Mermi_sayisi;
+    public int Noktasi;
+
+    void Start()
+    {
+
+         int gelenanahtar = Random.Range(0, silahlar.Length);
+
+         Olusan_Silahin_Turu = silahlar[gelenanahtar];
+         Olusan_Mermi_sayisi = mermiSayisi[Random.Range(0, mermiSayisi.Length)];
+
+        Silahin_resimi.sprite = Silah_resimleri[gelenanahtar];
+        
+      /*  Olusan_Silahin_Turu = "Taramali";
+        Olusan_Mermi_sayisi = 30;*/
+
+
+    }
+
+
+
+}
